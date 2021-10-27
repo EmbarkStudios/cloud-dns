dd15beff0afaa4342b1053d1f56d920885bf19d0
Fredrik Grönqvist <fredrik.gronqvist@embark-studios.com>
2021-10-27 12:18:45 +0200

    rustls

--- examples/list_dns_keys.rs
+++ examples/list_dns_keys.rs
@@ -1,5 +1,3 @@
-use hyper_tls::HttpsConnector;
-
 #[tokio::main]
 async fn main() -> cloud_dns::Result<()> {
     let project_id = std::env::var("PROJECT_ID").expect("PROJECT_ID env variable is required");
@@ -7,7 +5,7 @@ async fn main() -> cloud_dns::Result<()> {
         std::env::var("MANAGED_ZONE").expect("MANAGED_ZONE env variable is required");
 
     let service =
-        tower::ServiceBuilder::new().service(hyper::Client::builder().build(HttpsConnector::new()));
+        tower::ServiceBuilder::new().service(hyper::Client::builder().build(hyper_rustls::HttpsConnector::with_native_roots()));
 
     let response = cloud_dns::DnsClient::new(service, project_id.as_str())
         .dns_keys()
